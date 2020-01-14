983c45da1051902866b352a12b2966adc9d86177
Goran Mekić <meka@tilda.center>
2020-01-14 16:59:42 +0100

    Test thought

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import { DragDropContext } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
 import TouchBackend from 'react-dnd-touch-backend'
 import MultiBackend, { TouchTransition } from 'react-dnd-multi-backend'
+import { handleKeyboard } from '../shortcuts'
 
 const HTML5toTouch = {
   backends: [
@@ -23,12 +24,18 @@ const HTML5toTouch = {
 
 export const App = DragDropContext(MultiBackend(HTML5toTouch))(() =>
   <Provider store={store}>
-    <div onTouchMove={
-      () => globals.touching = true // eslint-disable-line no-return-assign
-    } onTouchEnd={() => {
-      globals.touching = false // eslint-disable-line no-return-assign
-      globals.touched = true // eslint-disable-line no-return-assign
-    }}>
+    <div
+      id="keyboard"
+      onTouchMove={
+        () => globals.touching = true // eslint-disable-line no-return-assign
+      }
+      onTouchEnd={() => {
+        globals.touching = false // eslint-disable-line no-return-assign
+        globals.touched = true // eslint-disable-line no-return-assign
+      }}
+      onKeyDown={handleKeyboard}
+      tabIndex="0"
+    >
       <AppComponent/>
     </div>
   </Provider>
--- src/components/__tests__/App.js
+++ src/components/__tests__/App.js
@@ -1,24 +1,58 @@
 import React from 'react'
 import { mount } from 'enzyme'
-import { act } from 'react-dom/test-utils'
 import { App } from '../App'
 
 // eslint-disable-next-line no-undef
 beforeAll(() => {
-  document.getSelection = () => ({ type: 'None' })
+  document.getSelection = () => ({
+    type: 'None',
+  })
+  document.createRange = () => ({
+    setStart: () => {},
+    setEnd: () => {},
+    commonAncestorContainer: {
+      nodeName: 'BODY',
+      ownerDocument: document,
+    },
+    collapse: () => {},
+  })
+  window.getSelection = () => ({
+    focusOffset: 3,
+    removeAllRanges: () => {},
+    addRange: () => {},
+  })
 })
 
 // eslint-disable-next-line no-undef
 afterAll(() => {
   // eslint-disable-next-line fp/no-delete
   delete document.getSelection
+  // eslint-disable-next-line fp/no-delete
+  delete window.getSelection
 })
 
 it('App', async () => {
-  let wrapper // eslint-disable-line fp/no-let,no-unused-vars
-  await act(async () => {
-    wrapper = await mount(
-      <App />
-    )
-  })
+  // eslint-disable-next-line no-undef
+  jest.useFakeTimers()
+  const wrapper = await mount(<App />, { attachTo: document.body })
+  const skipTutorial = wrapper.find('div.modal-actions div a')
+  skipTutorial.simulate('click')
+  const keyboardResponder = wrapper.find('div#keyboard')
+  await keyboardResponder.simulate('keydown', { key: 'Enter' })
+  // eslint-disable-next-line no-undef
+  jest.runAllTimers()
+  await wrapper.update()
+  const thoughts = wrapper.find('div.transformContain div ul')
+  const thought = thoughts.find(
+    'li.leaf div.thought-container div.thought div.editable',
+  )
+  // eslint-disable-next-line no-undef
+  expect(thought.text()).toBe('')
+  await thought.simulate('change', { target: { value: 'c' } })
+  await keyboardResponder.simulate('keydown', { key: 'Enter' })
+  // eslint-disable-next-line no-undef
+  jest.runAllTimers()
+  await thought.update()
+  // eslint-disable-next-line no-undef
+  expect(thought.text()).toBe('c')
 })
